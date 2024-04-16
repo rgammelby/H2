b5e747acc6261ba856b2ec57c58762ee0852a9ea
Sascha Rosalina B Gammelby <rfj594@ku.dk>
2024-04-16 08:44:13 +0200

    bmi calc polish

--- BMI udregneren/bmi.js
+++ BMI udregneren/bmi.js
@@ -1,32 +1,52 @@
+// calculates user BMI
+
 function bmiCalc() {
+
+    // variable declarations
     let height, weight, bmi, bmiDisplay;
 
+    // variable assignments. grabs the input fields 'height' and 'weight',
+    // as well as the paragraph tag which will contain the user's results.
     height = document.getElementById("height").value / 100;
     weight = document.getElementById("weight").value;
     bmiDisplay = document.getElementById("bmiDisplay");
 
+    // ensures the user has input both their height & their weight before calculating
     if (height === "" || weight === "") {
+
+        // fires an alert if the user has not supplied both height and weight
         alert("Please fill out your height and weight!");
         return;
     } else {
+        // does the actual calculation and assigns the 'bmi' variable declared earlier
         bmi = Math.round(weight/(height * 2));
+
+        // inserts the bmi value into the bmiDisplay paragraph tag
         bmiDisplay.textContent = "Your BMI: " + bmi;
 
+        // calls the method which will go into greater detail about the user's bmi result
+        // passes bmi as a variable, as this is used in the result() method
         result(bmi);
     }
 };
 
+// displays greater detail about the user's calculated bmi value
 function result(bmi) {
+
+    // grabs the elements 'resultRow', the hidden <tr> tag containing the detailed results paragraph
     let tr = document.getElementById("resultRow");
+    // and the 'result' paragraph tag itself
     let result = document.getElementById("result");
 
-console.log("BMI = " + bmi);
-
+    // resets the text contents and background colour of the 'result' tag for good measure
     result.textContent = "";
     result.style.backgroundColor = "none";
 
+    // declaration of detailed text result variable
     let textResult;
 
+    // sets an appropriate detailed message based on the user's calculated BMI
+    // also sets a colour
     if (bmi < 16) {
         result.style.backgroundColor = "red";
         textResult = "You are severely underweight. Consult a physician immediately.";
@@ -56,6 +76,9 @@ console.log("BMI = " + bmi);
         textResult = "You are obese - third degree.";
     }
 
+    // makes the table row tag containing this information visible
     tr.style.display = "block";
+
+    // sets the text content of the 'result' paragraph = the appropriate message
     result.textContent = textResult;
 };
