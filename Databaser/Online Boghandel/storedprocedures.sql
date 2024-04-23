ccf8c75aac0cc7c4d39cdcb8d2f558ffed784ea7
Sascha Rosalina B Gammelby <rfj594@ku.dk>
2024-04-23 13:21:15 +0200

    stored procedures separate file upload

--- Databaser/Online Boghandel/storedprocedures.sql
+++ Databaser/Online Boghandel/storedprocedures.sql
@@ -1,28 +1,8 @@
--- STORED PROCEDURES
-
--- insert statement for later procedures to work
-insert into Author
-values (default, 'Hejsa');
-
--- insert statement for GetInfoByBookTitle procedure to work
-insert into Genre
-values (default, 'Fantasy');
-
-insert into Book 
-values (default, 'Hej Bog', 1, 100, 1);
-
--- insert statement for GetCustomerInfoByCustomerName to work
-insert into City
-values (default, 'Dyssegård');
-insert into Postcode
-values (default, 2870);
-insert into Address
-values (default, 1, 1);
-insert into Customer
-values (default, 'Sascha', 's@mail.dk', 'Søborg Hovedgade 1', 1);
-
+-- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES
 
 -- fetches logs made in a span of dates 
+drop procedure if exists GetLogsBetweenDates;
+
 delimiter //
 create procedure GetLogsBetweenDates (IN firstDay INT, IN firstMonth INT, IN firstYear varchar(4), IN lastDay INT, IN lastMonth INT, IN lastYear varchar(4))
 begin
@@ -39,6 +19,8 @@ delimiter ;
 call GetLogsBetweenDates('23', '04', '', '27', '04', '');
 
 -- get books by author
+drop procedure if exists GetBooksByAuthor;
+
 delimiter //
 create procedure GetBooksByAuthor (in authorName varchar(256))
 begin 
@@ -53,6 +35,8 @@ delimiter ;
 call GetBooksByAuthor('Hejsa');
 
 -- get author of book
+drop procedure if exists GetAuthorByBookTitle;
+
 delimiter //
 create procedure GetAuthorByBookTitle (in bookTitle varchar(256)) 
 begin
@@ -66,14 +50,14 @@ delimiter ;
 call GetAuthorByBookTitle('Hej Bog');
 
 -- get customer info by customer
+drop procedure if exists GetCustomerInfoByCustomerName;
+
 delimiter //
 create procedure GetCustomerInfoByCustomerName (in customerName varchar(256))
 begin
-	select c.name as Name, c.email as 'E-mail', c.road_and_number as Address, ci.name as City, p.postcode as Postcode
+	select c.name as Name, c.email as 'E-mail', c.road_and_number as Address, a.postcode as 'Zip Code', a.city as City
     from Customer c
     join Address a on c.address = a.address_id
-    join City ci on a.city = ci.city_id
-    join Postcode p on a.postcode = p.postcode_id
     where c.name = customerName;
 end //
 delimiter ;
@@ -81,6 +65,8 @@ delimiter ;
 call GetCustomerInfoByCustomerName('Sascha');
 
 -- get orders by customer
+drop procedure if exists GetOrdersByCustomer;
+
 delimiter //
 create procedure GetOrdersByCustomer (in customerName varchar(256))
 begin
@@ -92,6 +78,8 @@ end //
 delimiter ;
 
 -- get book info by book
+drop procedure if exists GetBookInfoByBookTitle;
+
 delimiter //
 create procedure GetBookInfoByBookTitle (in bookTitle varchar(256))
 begin
@@ -105,4 +93,4 @@ delimiter ;
 
 call GetBookInfoByBookTitle('Hej Bog');
 
--- STORED PROCEDURES
+-- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES -- STORED PROCEDURES
